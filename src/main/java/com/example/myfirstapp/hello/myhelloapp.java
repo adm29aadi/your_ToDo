23cf7fced36cd398ffa71fff4d633785dd0907e6
adarsh <a1@gmail.com>
2024-05-19 23:10:08 +0530

    .jsp files used to server contains seperately

--- src/main/java/com/example/myfirstapp/hello/myhelloapp.java
+++ src/main/java/com/example/myfirstapp/hello/myhelloapp.java
@@ -5,12 +5,18 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-@Component
+@Controller
 public class myhelloapp {
     
 	@RequestMapping("Hello-you")
 	@ResponseBody
 	public String hello1() {
 		return "Hello!, how you doing today?";
+	}
+	
+	
+	@RequestMapping("Hello-html")
+	public String hello2() {
+		return "hello_world";
 	}
 }
---
+++ src/main/resources/META-INF/resources/WEB-INF/jsp/hello_world.jsp
@@ -0,0 +1,9 @@
+
+<html>
+<head>
+<title>Page Title</title>
+</head>
+<body>
+my name is adarsh and i am very that i made my first springboot and jpa project
+</body>
+</html>
